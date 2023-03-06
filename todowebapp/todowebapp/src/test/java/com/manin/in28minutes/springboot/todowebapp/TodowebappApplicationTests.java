56dcb2f55a38971b5c08a8185adf20c9b2010819
Manin <manin.t@jasmine.com>
2023-03-06 15:17:31 +0700

    add todowebapp

---
+++ todowebapp/todowebapp/src/test/java/com/manin/in28minutes/springboot/todowebapp/TodowebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manin.in28minutes.springboot.todowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodowebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
