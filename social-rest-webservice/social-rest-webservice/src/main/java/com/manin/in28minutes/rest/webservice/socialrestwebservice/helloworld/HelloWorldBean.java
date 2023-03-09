3bb30c213d7d5b2f0ad9db2754290aa0c4703c16
manin.t <manin.t@jasmine.com>
2023-03-09 16:51:33 +0700

    test commit from eclipse build rest api with spring boot

---
+++ social-rest-webservice/social-rest-webservice/src/main/java/com/manin/in28minutes/rest/webservice/socialrestwebservice/helloworld/HelloWorldBean.java
@@ -0,0 +1,23 @@
+package com.manin.in28minutes.rest.webservice.socialrestwebservice.helloworld;
+
+public class HelloWorldBean {
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+}
