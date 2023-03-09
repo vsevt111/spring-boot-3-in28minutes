660080b58b94936c287bfddfe7e5d646ece28962
manin.t <manin.t@jasmine.com>
2023-03-09 16:52:20 +0700

    initial build rest api with spring boot

---
+++ social-rest-webservice/social-rest-webservice/src/main/java/com/manin/in28minutes/rest/webservice/socialrestwebservice/SocialRestWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.manin.in28minutes.rest.webservice.socialrestwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialRestWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialRestWebserviceApplication.class, args);
+	}
+
+}
---
+++ social-rest-webservice/social-rest-webservice/src/test/java/com/manin/in28minutes/rest/webservice/socialrestwebservice/SocialRestWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manin.in28minutes.rest.webservice.socialrestwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialRestWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
